69dfa4814e9e3397828ee42b5d2277e0c08922cc
AGBEHOUNKPAN José <joseagbehounkpan@gmail.com>
2019-12-12 16:38:34 +0100

    add projects page

--- server.js
+++ server.js
@@ -17,9 +17,9 @@ app.use((req, res, next) => {
     next()
 })
 
-app.use((req, res, next) => {
+/* app.use((req, res, next) => {
     res.render('maitenance.hbs')
-})
+}) */
 
 app.use(express.static(__dirname + '/public'))
 
@@ -41,6 +41,12 @@ app.get("/", (req, res) => {
 app.get('/about', (req, res) => {
     res.render('about.hbs', {
         pageTitle: 'About page',
+    })
+})
+
+app.get('/projects', (req, res) => {
+    res.render('projects.hbs', {
+        pageTitle: 'Projects'
     })
 })
 
